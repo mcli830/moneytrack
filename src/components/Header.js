2a1ea3257a498aae1cf9ae05401ff7613cd14a49
Michael Li <mcli.893@gmail.com>
2019-07-09 14:13:48 -0700

    add mui-styles and added default-styled app header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
-export default props => {
+const defaultTheme = createMuiTheme();
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: defaultTheme.palette.primary.main,
+    color: defaultTheme.palette.background.default,
+    fontFamily: 'Lobster, serif',
+    padding: '0.5em 0',
+    textAlign: 'center',
+  }
+})
+
+export default function Header(props) {
+  const classes = useStyles();
   return (
     <div className="Header">
-      <Typography variant="h3">
+      <Typography variant="h4" className={classes.root}>
         MoneyTrack
       </Typography>
     </div>
