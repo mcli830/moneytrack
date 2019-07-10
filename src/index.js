118e52b543f38ce63b3ce39554555aa6286417d4
Michael Li <mcli.893@gmail.com>
2019-07-09 17:45:01 -0700

    add apollo-client

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 import 'typeface-roboto';
+import * as serviceWorker from './serviceWorker';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3000'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
