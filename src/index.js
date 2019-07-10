7004eb22e1511d1b61e1b3039b50a6ac35ae0871
Michael Li <mcli.893@gmail.com>
2019-07-10 10:54:23 -0700

    add graphql-tag dependency and test graphcool connection

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import 'typeface-roboto';
 import * as serviceWorker from './serviceWorker';
 
+import gql from 'graphql-tag';
+
 const httpLink = createHttpLink({
   uri: process.env.REACT_APP_GRAPHCOOL_URI
 });
@@ -18,7 +20,22 @@ const httpLink = createHttpLink({
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()
-})
+});
+ 
+// client.query({
+//   query: gql`
+//     query getUsers {
+//       allUsers {
+//         id
+//         name
+//         alias
+//         transactions {
+//           description
+//         }
+//       }
+//     }
+//   `
+// }).then(result => console.log(result));
 
 ReactDOM.render(
   <ApolloProvider client={client}>
