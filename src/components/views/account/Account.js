908a28b782c40271dd358f3dc331ef4c27f02057
Michael Li <mcli.893@gmail.com>
2019-07-27 16:49:55 -0700

    add account view options and stats

--- src/components/views/account/Account.js
+++ src/components/views/account/Account.js
@@ -3,12 +3,32 @@ import AccountView from './AccountView'
 
 export default (props) => {
 
-  const totalSpent = props.data.user.transactions.reduce((sum, t) => sum + t.amount, 0);
-
   return (
     <AccountView
       user={props.data.user}
-      totalSpent={totalSpent}
+      stats={analyzeData(props.data.user.transactions)}
     />
   )
+}
+
+
+// external helper
+function getMonthId(d){
+  return d.getUTCFullYear()*100 + d.getUTCMonth();
+}
+function analyzeData(data){
+  const dataset = {};
+  data.forEach(t => {
+    const monthId = getMonthId(new Date(t.date));
+    if (!Object.hasOwnProperty(monthId)){
+      dataset[monthId] = 0;
+    }
+    dataset[monthId] += t.amount;
+  });
+
+  const total = data.reduce((sum,t) => sum + t.amount, 0);
+  const monthly = parseFloat((total / Object.keys(dataset).length).toString()).toFixed(2);
+  const weekly = parseFloat(monthly * 0.25).toFixed(2);
+
+  return { total, monthly, weekly };
 }
--- src/components/views/account/AccountView.js
+++ src/components/views/account/AccountView.js
@@ -1,50 +1,132 @@
 import React from 'react'
 import Slide from '@material-ui/core/Slide'
 import Container from '@material-ui/core/Container'
-import Card from '@material-ui/core/Card'
-import CardHeader from '@material-ui/core/CardHeader'
-import CardContent from '@material-ui/core/CardContent'
-// import CardActions from '@material-ui/core/CardActions'
-import Avatar from '@material-ui/core/Avatar'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
+import Divider from '@material-ui/core/Divider'
 import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import FaceIcon from '@material-ui/icons/Face'
+import EmailIcon from '@material-ui/icons/Email'
+import EditIcon from '@material-ui/icons/Edit'
+// import CardActions from '@material-ui/core/CardActions'
 import { makeStyles } from '@material-ui/styles'
-import { createMuiTheme } from '@material-ui/core/styles'
+import { useTheme } from '@material-ui/core/styles'
+import { CURRENCY } from '../../../data/resolvers'
 
-const theme = createMuiTheme();
-
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
     backgroundColor: theme.palette.background.default,
-    color: theme.palette.primary.main,
-    padding: theme.spacing(2)
+    color: theme.palette.text.primary,
+    padding: theme.spacing(0,1),
+    '& *': {
+      listStyle: 'none',
+    }
   },
-  avatar: {
-
+  list: {
+    padding: 0,
   },
-  total: {
-    textAlign: 'center'
+  listIcon: {
+    color: theme.palette.grey[400],
+  },
+  listIconCurrency: {
+    fontSize: '1.5rem',
+    height: 24,
+    width: 24,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  listItem: {
+    minHeight: theme.spacing(8),
+    padding: theme.spacing(1,2),
+  },
+  listAction: {
+    color: theme.palette.grey[500],
+  },
+  listValue: {
+    color: theme.palette.grey[500],
+    paddingRight: theme.spacing(1.5),
   }
-})
+}))
 
-export default (props) => {
-  const classes = useStyles();
+function AccountView(props) {
+  const classes = useStyles(useTheme());
   return (
     <Slide in direction='right' mountOnEnter unmountOnExit>
       <Container maxWidth='sm' className={classes.root}>
-        <Card>
-          <CardHeader
-            title={props.user.name}
-            avatar={
-              <Avatar className={classes.avatar}>A</Avatar>
-            }
-          />
-        <CardContent>
-          <Typography variant='h2' className={classes.total}>
-            ${props.totalSpent}
-          </Typography>
-        </CardContent>
-        </Card>
+        <List className={classes.list}>
+          <AccountItem
+            text={props.user.name}
+            withIcon
+            icon={<FaceIcon/>}
+            includeAction
+            actionIcon={<EditIcon/>}
+            onAction={()=>console.log('Edit name')} />
+          <AccountItem
+            text={props.user.email}
+            withIcon
+            icon={<EmailIcon/>}
+            includeAction
+            actionIcon={<EditIcon/>}
+            onAction={()=>console.log('Edit email')} />
+          <AccountItem
+            text={props.user.currency}
+            withIcon
+            icon={<div className={classes.listIconCurrency}>{CURRENCY[props.user.currency]}</div>}
+            includeAction
+            actionIcon={<EditIcon/>}
+            onAction={()=>console.log('Edit currency')} />
+          <AccountItem
+            text='Number of Transactions'
+            onlyValue
+            value={props.user.transactions.length} />
+          <AccountItem
+            text='Total Expenses'
+            onlyValue
+            value={CURRENCY[props.user.currency] + props.stats.total} />
+          <AccountItem
+            text='Average monthly expenses'
+            onlyValue
+            value={CURRENCY[props.user.currency] + props.stats.monthly} />
+          <AccountItem
+            text='Average weekly expenses'
+            onlyValue
+            value={CURRENCY[props.user.currency] + props.stats.weekly} />
+        </List>
       </Container>
     </Slide>
-  )
+  );
+
+  // internal render helper
+  function AccountItem(props){
+    return (
+      <React.Fragment>
+        <ListItem className={classes.listItem}>
+          {props.withIcon && (
+            <ListItemAvatar className={classes.listIcon}>{props.icon}</ListItemAvatar>
+          )}
+          <ListItemText primary={props.text} secondary={props.subtext} className={classes.listText} />
+          {props.includeAction && (
+            <ListItemSecondaryAction>
+              <IconButton onClick={props.onAction} disabled={props.disableAction} className={classes.listAction}>
+                {props.actionIcon}
+              </IconButton>
+            </ListItemSecondaryAction>
+          )}
+          {(props.onlyValue && !props.includeAction ) && (
+            <ListItemSecondaryAction>
+              <div className={classes.listValue}>{props.value}</div>
+            </ListItemSecondaryAction>
+          )}
+        </ListItem>
+        <Divider light />
+      </React.Fragment>
+    );
+  }
 }
+
+export default AccountView;
